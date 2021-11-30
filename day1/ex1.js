669e7e1c4fd5a8c194f9a08ee6ee47ba5c4510f9
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-11-30 09:44:37 +0100

    Initial commit

---
+++ day1/ex1.js
@@ -0,0 +1,4 @@
+let num = 3;
+if((num % 2)===0){
+    console.log(`${num} la mot so chia het cho 2.`);
+}
---
+++ day1/exo1.js
@@ -0,0 +1,16 @@
+/*
+[
+    {field: 'name', value: 'Nicola'}
+]
+
+{ name: 'Nicola'}
+*/
+
+let myObject = [
+    {field: 'name', value: 'Nicola'}
+];
+
+function getRequestBodyFromValues(formValues) {
+        
+    
+}
